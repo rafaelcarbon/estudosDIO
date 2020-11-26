cc62959d796ce3305f0da0194d2abb2c26cb56fc
Rafael Oliveira <rafaelcarbon@outlook.com>
2020-11-26 15:48:41 -0300

    adicionando funções de movimento ao script

--- htmlwebdeveloper/snakegame/script/main.js
+++ htmlwebdeveloper/snakegame/script/main.js
@@ -6,18 +6,41 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
+let game = setInterval(start, 100);
 
-function criarBG(){
+function background(){
     context.fillStyle = "lightgray";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-function criarSnake(){
+function makeSnake(){
     for(i=0; i < snake.length; i++){
         context.fillStyle = "black";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
-criarBG();
 
-criarSnake();
+function start(){
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].Y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY += box;
+    if(direction == "down") snakeY -= box;
+
+    snake.pop();
+
+    let newSnake = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newSnake);
+
+    background();
+    makeSnake();
+}
+

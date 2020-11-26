b65b0c0c8d13910a923cb509a61454dc9b2134ba
Rafael Oliveira <rafaelcarbon@outlook.com>
2020-11-26 20:48:35 -0300

    Adicionando favicon, movimentos e transição de um lado para o outro

--- htmlwebdeveloper/snakegame/script/main.js
+++ htmlwebdeveloper/snakegame/script/main.js
@@ -2,45 +2,68 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
-snake[0] = {
-    x: 8 * box,
-    y: 8 * box
-}
 let direction = "right";
 let game = setInterval(start, 100);
 
-function background(){
-    context.fillStyle = "lightgray";
-    context.fillRect(0, 0, 16 * box, 16 * box);
+document.addEventListener("keydown", update);
+
+snake[0] = {
+
+    x: 3 * box,
+    y: 3 * box
+
 }
 
-function makeSnake(){
+
+function background(){
+
+    context.fillStyle = "lightgray";
+    context.fillRect(0, 0, 16 * box, 16 * box);
+
+}
+
+function makesnake(){
     for(i=0; i < snake.length; i++){
+
         context.fillStyle = "black";
         context.fillRect(snake[i].x, snake[i].y, box, box);
+
     }
 }
 
+function update(event){
+
+    if(event.keyCode == 37 && direction != "right") direction= "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function start(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    background();
+    makesnake();
 
     let snakeX = snake[0].x;
-    let snakeY = snake[0].Y;
+    let snakeY = snake[0].y;
+
 
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
-    if(direction == "up") snakeY += box;
-    if(direction == "down") snakeY -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
 
-    snake.pop();
-
-    let newSnake = {
+    let newsnake = {
         x: snakeX,
         y: snakeY
     }
 
-    snake.unshift(newSnake);
+    snake.pop();
+    snake.unshift(newsnake);
 
-    background();
-    makeSnake();
 }
 
